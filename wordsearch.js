659ecc508b798a73e882f037910e126e524d2caf
Wei Huang <huangwei19810803@gmail.com>
2023-05-10 15:12:26 -0600

    paring work

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,36 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    let find = false;
+    find = checkWord(horizontalJoin,word);
+    if (checkWord(horizontalJoin,word)) {
+      return true;
     }
-}
-
-module.exports = wordSearch
+    const verticalArray = getVerticalArray(letters);
+    const verticalJoin = verticalArray.map(ls=> ls.join(''));
+    find = checkWord(verticalJoin,word);
+    return find;
+  };
+  
+  const checkWord = (array, word) =>{
+    for (let l of array) {
+      if (l.includes(word)) return true;
+    }
+    return false;
+  };
+  
+  const getVerticalArray = (horizontalJoin)=> {
+   
+    let result = [];
+    for (let i = 0; i < horizontalJoin[0].length; i++) {
+      let vertical = [];
+      for (let j = 0; j < horizontalJoin.length; j++) {
+        vertical.push(horizontalJoin[j][i]);
+      }
+      result.push(vertical);
+    }
+  
+    return result;
+  };
+  
+  
+  module.exports = wordSearch;
